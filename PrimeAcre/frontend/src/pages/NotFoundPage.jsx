0f93d89b1a70600f743cf30c649c1eac15563965
Faheem <faheemuddinsayyed789@gmail.com>
2024-10-25 19:36:59 +0530

    Fixed 404 Not Found Page

--- PrimeAcre/frontend/src/pages/NotFoundPage.jsx
+++ PrimeAcre/frontend/src/pages/NotFoundPage.jsx
@@ -1,10 +1,25 @@
 // src/pages/NotFoundPage.jsx
 
 import React from 'react';
+import {Box} from '@mui/material';
 import './NotFoundPage.css'; // Ensure the CSS file is in the same directory
 
 const NotFoundPage = () => {
   return (
+
+    <Box
+            sx={{
+                backgroundColor: '#121212',
+                color: '#ffffff',
+                minHeight: '100vh',
+                width: '100vw',
+                display: 'flex',
+                alignItems: 'center', // Center vertically
+                justifyContent: 'center', // Center horizontally
+                p: 3,
+            }}
+      >
+    
     <div className="not-found-body">
       <div className="head">
         <div className="meta"></div>
@@ -15,6 +30,7 @@ const NotFoundPage = () => {
         <div className="error-text">Got lost? How.....?  why.....?  Ahhhh....</div>
       </div>
     </div>
+    </Box>
   );
 };
 
